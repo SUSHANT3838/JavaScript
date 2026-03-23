4cefc7630e309313112ae228e90d699c90927b5a
SUSHANT3838 <sushantshete876@gmail.com>
2026-03-23 07:53:35 +0530

    update

--- function/script2.js
+++ function/script2.js
@@ -1,8 +1,20 @@
+// Pen info
 const product = {
     name : "Pen",
     rating : 4,
     offer : 20,
     price : 100
 };
-
 console.log(product);
+
+// insta profile info
+const insta_prof = {
+    userNmae : "sushantshete001",
+    name : "Sushant",
+    isFollow : true,
+    numOfPost : 10,
+    numOfFollowers : 190,
+    numOfFollowing : 180,
+    bio : "Simple person"
+};
+console.log(insta_prof)
