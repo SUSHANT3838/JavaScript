4d5b62ab5146f31e8bfe08c2efbe433d7afb9fac
SUSHANT3838 <sushantshete876@gmail.com>
2026-03-23 15:36:38 +0530

    Update

--- function/script4.js
+++ function/script4.js
@@ -1,4 +1,30 @@
 // new method of declare the function
-const square = n => n * n;
+const square = n => n * n; // arrow function (ES6)
 let sq = square(4);
 console.log(`The square of 4 is ${sq}`);
+
+// IIFE used for creating private variable
+var counter = (function(){
+    var count = 0;// private
+
+    return {
+        increament : function(){
+            count++;
+        },
+        decreament : function(){
+            count--;
+        },
+        getCount : function(){
+            return count;
+        }
+    };
+})();
+
+counter.increament();
+counter.increament();
+counter.increament();
+
+console.log(`The total count is ${counter.getCount()}`);
+
+// not allow to direct access of count
+console.log(counter.count);// undefined ->because it is private variable
