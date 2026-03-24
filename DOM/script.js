173f4f9a7252d67daf76e7972c059e60d7ab9780
SUSHANT3838 <sushantshete876@gmail.com>
2026-03-24 09:13:45 +0530

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,10 +1,13 @@
+// access all divs which has class 'box'
 let divs = document.querySelectorAll(".box");
+// change the inner text using index
 idx = 0;
-
 for(div of divs){
     divs[idx].innerText = "Hello";
     console.log(divs[idx]);
     idx++;
 }
 
-
+let heding = document.querySelector("h1");
+heding.innerText = heding.innerText + " to my website";
+console.log(heding);
